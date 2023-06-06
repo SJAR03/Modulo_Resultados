2d50250cf0df79acf5717f7093b746212e1f842c
eduardo <eduardojose13579@gmail.com>
2023-06-05 22:33:10 -0600

    cors usuario

--- businesdomain/usuario/src/main/java/com/prolis/usuario/UsuariosAplication.java
+++ businesdomain/usuario/src/main/java/com/prolis/usuario/UsuariosAplication.java
@@ -2,12 +2,28 @@ package com.prolis.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UsuariosAplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UsuariosAplication.class, args);
+    }
+
+}
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
--- businesdomain/usuario/src/main/java/com/prolis/usuario/controller/UsuarioController.java
+++ businesdomain/usuario/src/main/java/com/prolis/usuario/controller/UsuarioController.java
@@ -4,13 +4,13 @@ import com.prolis.usuario.service.impl.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/usuario")
